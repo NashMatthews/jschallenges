fce0e8f98440af9180a44a6d43c38120c51f8ce2
NashMatthews <mwnash@protonmail.com>
2022-12-12 14:20:58 +0000

    firstcommit

---
+++ arrays.js
@@ -0,0 +1,20 @@
+let favHolidayDest = [
+    "Copenhagen, Denmark",
+    "Brussels, Belgium",
+    "Tolmin, Slovenia"
+]
+
+let favFilms = [
+    "Fellowship of the Ring",
+    "The Two Towers",
+    "The Return of the King"
+]
+
+favHolidayDest[1] = "Koh Tao, Thailand"
+
+// console.log(`I enjoy watching ${favFilms[0]} and ${favFilms[1]} in ${favHolidayDest[0]}, 
+// but ${favFilms[2]} is better enjoyed in ${favHolidayDest[1]}. No films in ${favHolidayDest[2]} 
+// though. There, it's all about the scenery`);
+
+console.log(favFilms.pop())
+console.log(favFilms)
