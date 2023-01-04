cb626b454a0e7442661c35811f85cbef12015e60
NashMatthews <mwnash@protonmail.com>
2023-01-04 22:40:24 +0000

    CyberPet Merge

--- cyberpet/Desk 2 CyberPet/CyberPet Merge/cyberpet23-12.js
+++ cyberpet/Desk 2 CyberPet/CyberPet Merge/cyberpet23-12.js
@@ -10,11 +10,9 @@
 
 /* UPDATE!!!
 
-Kept the game loop from starting as soon as pet name is submitted
+Game seems to be running fine now.
 
-Hunger/Fatigue/Happiness/Wool/Shoe levels aren't increasing/decreasing
-
-When horse button is clicked and game is started it still runs the sheep game loop
+We took the random numbers out to make it simpler to figure out the niggles but I've switched them back to random and they're working.
 
 */
 
@@ -30,9 +28,29 @@ const start = document.getElementById("start");
 const list = document.getElementById("list");
 const feed = document.getElementById("feed");
 const play = document.getElementById("play");
-const sleep = document.getElementById("sleep");
+const sleep = document.getElementById("sleep"); 
 const shear = document.getElementById("shear");
 const change = document.getElementById("change");
+const music1 = document.getElementById("music1");
+const music2 = document.getElementById("music2");
+const gameovertheme = document.getElementById("gameover");
+
+function maintheme() {
+  music1.volume = 0.08;
+  music1.play();
+}
+
+function gametheme() {
+  music2.volume = 0.4;
+  music2.play();
+  music1.pause();
+}
+
+function gameover() {
+  music2.pause();
+  gameovertheme.volume = 0.2;
+  gameovertheme.play();
+}
 
 // declare initial BasePet class
 
@@ -66,8 +84,8 @@ class Sheep extends BasePet {
 // set the extended class constructor for Horse class
 
 class Horse extends BasePet {
-    constructor(name, animal, hunger, fatigue, happiness, horseShoeHealth) {
-      super(name, animal, hunger, fatigue, happiness);
+    constructor(name, animal, hunger, fatigue, happiness, stillAlive, horseShoeHealth) {
+      super(name, animal, hunger, fatigue, happiness, stillAlive);
       this.horseShoeHealth = horseShoeHealth;
     }
 
@@ -80,11 +98,9 @@ class Horse extends BasePet {
 
   const petHorse = new Horse("", "Horse", 50, 50, 50, true, 50);
 
-
-
 // declare necessary variables for later in the code
 
-let blahblah;
+let animals;
 let intervalTimerIndividual;
 
 // hide all animal selection and action buttons
@@ -98,13 +114,12 @@ play.style.visibility = "hidden";
 shear.style.visibility = "hidden";
 change.style.visibility = "hidden";
 
-// Only cycle through the script up to this point until the 'Submit' button is pressed????????????????
-
-// Choose name input
+// STEP 1 - Input pet's name and click 'Submit'
 
 submit.addEventListener("click", () =>{
+    maintheme();
     petName = input.value;
-    placeholder.textContent = `Your pet's name is ${petName}! What a fantastic name! Is it a sheep or a horse? Those are your options`;
+    placeholder.textContent = `Your pet's name is ${petName}! What a fantastic name! Now, is ${petName} a sheep or a horse? Those are your options`;
     submit.style.display = "none";
     input.style.display = "none";
     sheep.style.visibility = "visible";
@@ -112,170 +127,199 @@ submit.addEventListener("click", () =>{
     new BasePet(petName, "", 50, 50, 50, true);
 });
 
-// Only cycle through the script up to this point until an animal selecter button is clicked??????????????????
-
-/* If you click the 'Sheep' button, the animal buttons disappear
-  a message appears
-  a new 'Sheep' class is declared using the class extender
+/* STEP 2 - Pick whether your pet is a sheep or a horse
+        sheepish/horseish functions declare the value of the 'animals' variable
+        these functions are called when the corresponding button is clicked
 */
+
+const sheepish = () => {
+  animals = "Sheep";
+}
+const horseish = () => {
+  animals = "Horse";
+}
 
 sheep.addEventListener("click", () =>{
     placeholder.textContent = `${petName} the sheep!? Baaa-utiful!`;
     start.style.visibility = "visible";
-    feed.style.visibility = "visible";
-    sleep.style.visibility = "visible";
-    play.style.visibility = "visible";
-    shear.style.visibility = "visible";
     sheep.style.visibility = "hidden";
     horse.style.visibility = "hidden";
-    
-    window.blahblah = "Sheep";
+    sheepish();
 });
 
-/* If you click the 'Horse' button, the animal buttons disappear
-  a message appears
-  a new 'Horse' class is declared using the class extender
+/* a new 'Horse' class is declared using the class extender... I THINK???
 */
 
 horse.addEventListener("click", () =>{
     placeholder.textContent = `Is ${petName} a sheep? Neigh, it's a horse!!!`;
     start.style.visibility = "visible";
-    feed.style.visibility = "visible";
-    sleep.style.visibility = "visible";
-    play.style.visibility = "visible";
-    change.style.visibility = "visible";
     sheep.style.visibility = "hidden";
-    horse.style.visibility = "hidden";
-    
-    window.blahblah = "Horse"
+    horse.style.visibility = "hidden";  
+    horseish();
 });
 
-/* "Start Game" button: Rather than the game starting when the name is input, or when the animal is selected,
-  it will start when this button is clicked.
-  A different function should execute depending on which animal has been selected
+/* STEP 3 - Click 'Start Game' button
+
+if animal is sheep then the 'Shear' button appears; if horse then the 'Change Shoes' button appears
+different loop starts depending on the animal
 */
 
+start.addEventListener("click", () =>{
+  feed.style.visibility = "visible";
+  sleep.style.visibility = "visible";
+  play.style.visibility = "visible";
+  gametheme()
 
+  if (animals == "Sheep"){
+    shear.style.visibility = "visible";
+    gameLoopSheep();
+    // intervalTimerIndividual = setInterval(gameLoopSheep(), 1000);
+  }  
+  else if (animals == "Horse"){
+    change.style.visibility = "visible";
+    gameLoopHorse();
+    // intervalTimerIndividual = setInterval(gameLoopHorse(), 2000);
+  }
+})
 
-/* Game loop for sheep begins
-  cheacks if sheep is still alive
-  if it is, it increase hunger, fatigue and wool length levels by random amounts each second
-  decreases happiness amount
-  if sheep is dead, return message
-
-  This should also display the hunger, fatigue, happiness and wool length levels onscreen
+/* These two functions are run by the game loop function below them
+          render function to display stats
+          timing function to add/subtract from stat levels, check if the pet is still alive, then run the game loop function again
 */
 
-const gameLoopSheep = () => {
-  if (petSheep.stillAlive == true){
-
-    stats.textContent = `${petName}'s hunger level is: ${petSheep.hunger}
+const renderSheep = () => {
+  stats.textContent = `${petName}'s hunger level is: ${petSheep.hunger}
                         ${petName}'s fatigue level is: ${petSheep.fatigue}
                         ${petName}'s happiness level is: ${petSheep.happiness}
                         ${petName}'s wool length is: ${petSheep.woolLength}`
+}
 
+const timingFunctionSheep = () => {
+  window.setTimeout(() => {
+    // petSheep.hunger += 5
+    // petSheep.fatigue += 5
+    // petSheep.happiness -= 5
+    // petSheep.woolLength += 5
+    petSheep.hunger += Math.floor(Math.random()*10)+3
+    petSheep.fatigue += Math.floor(Math.random()*10)+3
+    petSheep.happiness -= Math.floor(Math.random()*10)+3
+    petSheep.woolLength += Math.floor(Math.random()*10)+3
+    petSheep.checkIfAliveSheep();
+    gameLoopSheep();
+  }, 2000)
+}
+
+/* Game loop function
+          if sheep is still alive it runs render function, then timing function (see above)
+          if sheep is dead it returns a different game over message depending on how the sheep died
+*/
+
+const gameLoopSheep = () => {
+  
+  if (petSheep.stillAlive == true){
+    
+    renderSheep()  
+    timingFunctionSheep()
+      
     // console.log(`${petName}'s hunger level is: ${petSheep.hunger}`);
     // console.log(`${petName}'s fatigue level is: ${petSheep.fatigue}`);
     // console.log(`${petName}'s happiness level is: ${petSheep.happiness}`);
     // console.log(`${petName}'s wool length is: ${petSheep.woolLength} units`);
-
-    petSheep.hunger += 5
-    // petSheep.hunger += Math.floor(Math.random()*10)+1
-    petSheep.fatigue += 5
-    petSheep.happiness -= 5
-    petSheep.woolLength += 5
-    // petSheep.fatigue += Math.floor(Math.random()*10)+1
-    // petSheep.happiness -= Math.floor(Math.random()*10)+1
-    // petSheep.woolLength += Math.floor(Math.random()*10)+1
-
-    petSheep.checkIfAliveSheep()
+    
   }
   else if (petSheep.stillAlive == false){
-    console.log(`GAME OVER! You idiot! ${petName} is dead and it's all your fault.`)
-    window.alert(`GAME OVER! You idiot! ${petName} is dead and it's all your fault.`)
+    gameover();
+    // console.log(`GAME OVER! You idiot! ${petName} is dead and it's all your fault.`)
+    if (petSheep.hunger >= 100){
+      window.alert(`GAME OVER! ${petName} didn't get enough food and they have starved to death!`)
+    }
+    else if (petSheep.fatigue >= 100){
+      window.alert(`GAME OVER! ${petName} is just too tired to go on.`)
+    }
+    else if (petSheep.happiness <= 0){
+      window.alert(`GAME OVER! ${petName}'s emotional needs have been neglected by you, and as a result, they have been crushed under the weight of their own sadness`)
+    }
+    else if (petSheep.woolLength >= 100){
+      window.alert(`GAME OVER! ${petName} has been suffocated by their overgrown wool!`)
+    }
+    // window.alert(`GAME OVER! You idiot! ${petName} is dead and it's all your fault.`)
     clearInterval(intervalTimerIndividual)
   }
 }
 
-/* Game loop for horse begins
-  cheacks if horse is still alive
-  if it is, it increase hunger and fatigue levels by random amounts each second
-  decreases happiness and horse shoe health amount
-  if horse is dead, return message
+// same as above but for horse
 
-  This should also display the hunger, fatigue, happiness and horse shoe health levels onscreen
-*/
-
-const gameLoopHorse = () => {
-  if (petHorse.stillAlive == true){
-
-    stats.textContent = `${petName}'s hunger level is: ${petHorse.hunger}
+const renderHorse = () => {
+  stats.textContent = `${petName}'s hunger level is: ${petHorse.hunger}
                         ${petName}'s fatigue level is: ${petHorse.fatigue}
                         ${petName}'s happiness level is: ${petHorse.happiness}
                         ${petName}'s horse shoe health level is: ${petHorse.horseShoeHealth}`
+}
+
+const timingFunctionHorse = () => {
+  window.setTimeout(() => {
+    // petHorse.hunger += 5
+    // petHorse.fatigue += 5
+    // petHorse.happiness -= 5
+    // petHorse.horseShoeHealth -= 5
+    petHorse.hunger += Math.floor(Math.random()*10)+3
+    petHorse.fatigue += Math.floor(Math.random()*10)+3
+    petHorse.happiness -= Math.floor(Math.random()*10)+3
+    petHorse.horseShoeHealth -= Math.floor(Math.random()*10)+3
+    petHorse.checkIfAliveHorse();
+    gameLoopHorse();
+  }, 2000)
+}
+const gameLoopHorse = () => {
+  if (petHorse.stillAlive == true){
+    renderHorse();
+    timingFunctionHorse();
 
       // console.log(`${petName}'s hunger level is: ${petHorse.hunger}`);
       // console.log(`${petName}'s fatigue level is: ${petHorse.fatigue}`);
       // console.log(`${petName}'s happiness level is: ${petHorse.happiness}`);
       // console.log(`${petName}'s horse shoe health level is: ${petHorse.horseShoeHealth}`);
   
-      petHorse.hunger += 5
-      petHorse.fatigue += 5
-      petHorse.happiness -= 5
-      petHorse.horseShoeHealth -= 5
-      // petHorse.hunger += Math.floor(Math.random()*10)+1
-      // petHorse.fatigue += Math.floor(Math.random()*10)+1
-      // petHorse.happiness -= Math.floor(Math.random()*10)+1
-      // petHorse.horseShoeHealth -= Math.floor(Math.random()*10)+1
-
-      petHorse.checkIfAliveHorse()
   }
   else if (petHorse.stillAlive == false){
-    console.log("GAME OVER! Pet horse is dead!")
+    gameover();
+    if (petHorse.hunger >= 100) {
+      window.alert(`GAME OVER! You didn't feed ${petName} enough and they died. Nice one.`);
+    }
+    else if (petHorse.fatigue >= 100) {
+      window.alert(`GAME OVER! ${petName} is so knackered that they've keeled over and died.`);
+    }
+    else if (petHorse.happiness <= 0) {
+      window.alert(`GAME OVER! ${petName} is a sad horse. At least, they would be if they hadn't just died of sadness. I hope you're pleased with yourself.`);
+    }
+    else if (petHorse.horseShoeHealth <= 0) {
+      window.alert(`GAME OVER! You didn't change ${petName}'s shoes and their feet got infected. The infection spread through their body and they died in agony.`)
+    }
+    // console.log("GAME OVER! Pet horse is dead!")1
     // window.alert("GAME OVER! Your pet is dead!")
     clearInterval(intervalTimerIndividual)
   }
 }
 
-start.addEventListener("click", () =>{
-
-  if (blahblah == "Sheep"){
-    gameLoopSheep();    
-  }
-  
-  else if (blahblah == "Horse"){
-    gameLoopHorse();
-  }
-})
-
-// Running the gameloop for you chosen pet (do we need an if/else statement? both do the same thing)
-
-if (blahblah = "Sheep"){
-    intervalTimerIndividual = setInterval(gameLoopSheep(), 1000);
-  }
-else {
-    intervalTimerIndividual = setInterval(gameLoopHorse(), 2000);
-  }
-
 /* Action buttons for the game
-  feed and sleep buttons affect different classes (Sheep, Horse) depending on the animal selected (if/else statement)
-  if 'Feed' or 'Sleep' buttons are clicked, hunger or fatigue decrease by random amount between five and ten
-  if 'Play' or 'Change Horse Shoes' buttons are clicked, happiness or horse shoe health increase by random amount between five and ten
+  feed, play and sleep buttons affect different classes (Sheep, Horse) depending on the animal selected (if/else statement)
+  if 'Feed', 'Sleep' or 'Shear' buttons are clicked, hunger, fatigue or woolLength decrease by random amount between five and fifteen
+  if 'Play' or 'Change Horse Shoes' buttons are clicked, happiness or horse shoe health increases by random amount between five and fifteen
   when each button is clicked it is disabled for a random amount of time, up to five seconds
 */
 
 feed.addEventListener('click', () => {
-  if (blahblah = "Sheep"){
-    petSheep.hunger -= 30;
-    // Sheep.hunger -= Math.floor(Math.random()*10)+5;
+  if (animals == "Sheep"){
+    // petSheep.hunger -= 30;
+    petSheep.hunger -= Math.floor(Math.random()*10)+5;
   }
-  else {
-    petHorse.hunger -= 30;
-    // Horse.hunger -= Math.floor(Math.random()*10)+5;
+  else if (animals == "Horse"){
+    // petHorse.hunger -= 30;
+    petHorse.hunger -= Math.floor(Math.random()*10)+5;
   };
 
-  const reloadTime = 3;
-  // const reloadTime = Math.floor(Math.random() * 5) + 1;
+  // const reloadTime = 3;
+  const reloadTime = Math.floor(Math.random() * 5) + 1;
 
 
   feed.disabled = true;
@@ -287,17 +331,17 @@ feed.addEventListener('click', () => {
 
 
 play.addEventListener('click', () => {
-    if (blahblah = "Sheep"){
-      petSheep.happiness += 20
-      // petSheep.happiness += Math.floor(Math.random()*10)+5;
+    if (animals == "Sheep"){
+      // petSheep.happiness += 20
+      petSheep.happiness += Math.floor(Math.random()*10)+5;
     }
-    else {
-      petHorse.happiness += 20
-      // petHorse.happiness += Math.floor(Math.random()*10)+5;
+    else if (animals == "Horse"){
+      // petHorse.happiness += 20
+      petHorse.happiness += Math.floor(Math.random()*10)+5;
     };
   
-    const reloadTime = 2
-    // const reloadTime = Math.floor(Math.random() * 5) + 1;
+    // const reloadTime = 2
+    const reloadTime = Math.floor(Math.random() * 5) + 1;
   
     play.disabled = true;
   
@@ -308,17 +352,17 @@ play.addEventListener('click', () => {
 
 
 sleep.addEventListener('click', () => {
-    if (blahblah = "Sheep"){
-      petSheep.fatigue -= 2;
-      // petSheep.fatigue -= Math.floor(Math.random()*10)+5;
+    if (animals == "Sheep"){
+      // petSheep.fatigue -= 2;
+      petSheep.fatigue -= Math.floor(Math.random()*10)+5;
     }
-    else {
-      petHorse.fatigue -= 2;
-      // petHorse.fatigue -= Math.floor(Math.random()*10)+5;
+    else if (animals == "Horse"){
+      // petHorse.fatigue -= 2;
+      petHorse.fatigue -= Math.floor(Math.random()*10)+5;
     };
   
-    const reloadTime = 2;
-    // const reloadTime = Math.floor(Math.random() * 5) + 1;
+    // const reloadTime = 2;
+    const reloadTime = Math.floor(Math.random() * 5) + 1;
   
     sleep.disabled = true;
   
@@ -329,11 +373,11 @@ sleep.addEventListener('click', () => {
 
 
 shear.addEventListener('click', () => {
-    petSheep.woolLength -= 3;
-    // petSheep.woolLength -= Math.floor(Math.random() * 10) + 5;
+    // petSheep.woolLength -= 3;
+    petSheep.woolLength -= Math.floor(Math.random() * 10) + 5;
   
-    const reloadTime = 3;
-    // const reloadTime = Math.floor(Math.random() * 5) + 1;
+    // const reloadTime = 3;
+    const reloadTime = Math.floor(Math.random() * 5) + 1;
   
     shear.disabled = true;
   
@@ -344,11 +388,11 @@ shear.addEventListener('click', () => {
 
 
   change.addEventListener('click', () => {
-    petHorse.horseShoeHealth += 3;
-    // petHorse.horseShoeHealth += Math.floor(Math.random()*10)+5;
+    // petHorse.horseShoeHealth += 3;
+    petHorse.horseShoeHealth += Math.floor(Math.random()*10)+5;
 
-    const reloadTime = 3;
-    // const reloadTime = Math.floor(Math.random() * 5) + 1;
+    // const reloadTime = 3;
+    const reloadTime = Math.floor(Math.random() * 5) + 1;
   
     change.disabled = true;
   
